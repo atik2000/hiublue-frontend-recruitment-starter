029b3d9be8c20f81f924684ecd6baf05cc80d918
atik2000 <atik@whitepaper.tech>
2025-02-22 19:37:31 +0600

    refactor: improve theme type safety and code formatting

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,41 +1,44 @@
-'use client';
+"use client";
 
-import {useMemo} from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import {createTheme, ThemeProvider as MuiThemeProvider} from '@mui/material/styles';
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  createTheme,
+  ThemeProvider as MuiThemeProvider,
+} from "@mui/material/styles";
+import { useMemo } from "react";
 
 // system
-import {palette} from './palette';
-import {shadows} from './shadows';
-import {typography} from './typography';
-import {customShadows} from './custom-shadows';
+import { customShadows } from "./custom-shadows";
+import { palette } from "./palette";
+import { shadows } from "./shadows";
+import { typography } from "./typography";
 // options
-import componentsOverrides from './overrides';
+import componentsOverrides from "./overrides";
 
 // ----------------------------------------------------------------------
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const baseTheme = createTheme({
-    palette,
-    typography,
-    shadows: shadows(),
-    customShadows: customShadows(),
-});
+  palette,
+  typography,
+  shadows: shadows(),
+  customShadows: customShadows(),
+} as any);
 
-export default function ThemeProvider({children}: Props) {
-    const theme = useMemo(() => {
-        const newTheme = createTheme(baseTheme);
-        newTheme.components = componentsOverrides(newTheme);
-        return newTheme;
-    }, []);
+export default function ThemeProvider({ children }: Props) {
+  const theme = useMemo(() => {
+    const newTheme = createTheme(baseTheme);
+    newTheme.components = componentsOverrides(newTheme) as any;
+    return newTheme;
+  }, []);
 
-    return (
-        <MuiThemeProvider theme={theme}>
-            <CssBaseline/>
-            {children}
-        </MuiThemeProvider>
-    );
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MuiThemeProvider>
+  );
 }
